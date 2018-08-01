287953c7a121a23c85e8e0fca319ecb413514938
aboud20-meet <aboud20@meet.mit.edu>
2018-08-01 16:49:06 +0300

    dddd

--- html-events/html-events.js
+++ html-events/html-events.js
@@ -1,1 +1,6 @@
 // Put all your JavaScript in this file!
+var b = document.getElementsByTagName("body")
+function changeBG() {
+	var x = prompt('what is your favorite colour?')
+	b[0].style.backgroundColor = x
+}
--- object-literals/object-literals.js
+++ object-literals/object-literals.js
@@ -1,1 +1,10 @@
 // Create your 'me' object literal here!
+var me = {
+	firstname: "Idan",
+	lastname: "Zamir",
+	favoritefoods: ["pasta", "salad", "omlet"],
+	age: 16,
+	picture: "www.jesuschristsavior.net/Savior.jpeg",
+}
+
+document.write(JSON.stringify(me));
